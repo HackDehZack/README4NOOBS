d3b154724e33fb667e73209857cf3b3d56491fab
HackDehZack <Zackseriousemail@gmail.com>
2023-11-13 21:10:21 -0500

    Setup

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,72 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    { 
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    { 
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    { 
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.',
+    },
+    { 
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information.',
+    },
+    { 
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines.',
+    },
+    { 
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions.',
+    },
+    { 
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license.',
+        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+    },
+    { 
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    { 
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+]; 
+
+
 
 // TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+
+function init() {
+    inquirer.prompt(questions)
+    .then((response) => {
+        console.log(response);
+})
+}
 
 // Function call to initialize app
 init();
